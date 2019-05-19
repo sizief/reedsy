06d4d24a12e4d5ef4c0ed7a4afd54c95fbe0c1d8
sizief <sizief@gmail.com>
2019-05-20 02:27:37 +0430

    remove extra error message

--- lib/errors/object_record/model_error.rb
+++
@@ -1,9 +0,0 @@
-module Errors
-  module ObjectRecord
-    class ModelError < StandardError
-      def initialize
-        super('You need to save one object to initialize this model on Object Record ')
-      end
-    end
-  end
-end
